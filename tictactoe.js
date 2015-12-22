c8c7943c8a751a558b8dc3beec61b2410a054279
Dylan Porteus <dylanporteus@aol.com>
2015-12-21 20:08:21 -0500

    revised code with comments

--- tictactoe.js
+++ tictactoe.js
@@ -25,8 +25,8 @@
    var game_slate = [
     
     [box1, box2, box3],
-    [box4, box5, box6],
-    [box7, box8, box9]
+    [box4, box5, box6],    //Contains the variables for each of the nine divs that 
+    [box7, box8, box9]     //make up the board.
 
    ];
 
@@ -40,16 +40,17 @@
    var movesCount = 0;
 
 
-  overlay.hide();
+  overlay.hide();   //The overlay is automatically hidden at the beginning of the game,
+                    //or else it will be unplayable.
 
 
       $('.abox').click(function(){
    	   if (this.classList.contains('hasBeenPicked') == false){
 	   	if (playerTurn === 1){
-	   		var playerSun = $("<div class='Sun'></div>");
-	   		$(this).addClass('hasBeenPicked')
-	     	$(this).append(playerSun);
-   			playerTurn = 2;
+	   		var playerSun = $("<div class='Sun'></div>");      //This function ensures that each box will be clickable,
+	   		$(this).addClass('hasBeenPicked')                  //that it will always alternate (thanks to playerTurn) between
+	     	$(this).append(playerSun);                         //the Sun and Moon. They are appended as classes into the
+   			playerTurn = 2;                                    //game_slate wrapper.
         movesCount++;
         getWinner();
 	   	}else{
@@ -76,8 +77,8 @@
         console.log('checkrows called')
         for(var i = 0; i < game_slate.length; i++){
           var count = 0;
-          for(var k = 0; k < game_slate.length; k++){
-
+          for(var k = 0; k < game_slate.length; k++){             //This function will check the game_slate array
+                                                                  //for three of the same symbol lined up in any row. 
             if($('div', game_slate[i][k]).hasClass('Sun')){
               count++;
             }else if($('div', game_slate[i][k]).hasClass('Moon')){
@@ -99,8 +100,8 @@
 
       var checkCols = function(){
         console.log('checklos called')
-        for(var i = 0; i < game_slate.length; i++){
-          var count = 0;
+        for(var i = 0; i < game_slate.length; i++){             //This function will check the game_slate array
+          var count = 0;                                        //for three of the same symbol lined up in any column.    
           for(var c = 0; c < game_slate.length; c++){
             if($('div', game_slate[c][i]).hasClass('Sun')){
               count++;
@@ -122,8 +123,8 @@
 
       var checkDiag = function(){
         console.log('checkdiag called')
-        if($('div' ,game_slate[0][0]).hasClass('Sun') &&
-           $('div' ,game_slate[1][1]).hasClass('Sun') &&
+        if($('div' ,game_slate[0][0]).hasClass('Sun') &&    //This function will check the game_slate array
+           $('div' ,game_slate[1][1]).hasClass('Sun') &&    //for three of the same symbol lined up in either diagonal.
            $('div' ,game_slate[2][2]).hasClass('Sun') ||
            $('div' ,game_slate[0][2]).hasClass('Sun') &&
            $('div' ,game_slate[1][1]).hasClass('Sun') &&
@@ -152,19 +153,19 @@
         if(movesCount >= 5){
           if(checkRows() !== undefined){
              var winner = checkRows();
-             // console.log("checkrows: winner is "+winner+"");
+             
              declareWinner(winner);
-             return winner
+             return winner                           //Winner will be either "Day" (for three suns), "Night" (for three moons),
+          }else if(checkCols() !== undefined){        //or "Neither" in the case of a tie.
             
-          }else if(checkCols() !== undefined){
             var winner = checkCols();
-             // console.log("checkcols: winner is "+winner+"");
+             
              declareWinner(winner);
             return winner
 
           }else if(checkDiag() !== undefined){
               var winner = checkDiag();
-              //console.log("checkDiag: winner is "+winner+"");
+              
               declareWinner(winner);
                docOverlay();
             return winner
@@ -178,15 +179,17 @@
    var declareWinner = function(winner){
         
        
-      
+      // if(winner === "Day"){
+      //   $('.sun-win').fadeIn("slow");
+      // }
 
      
        var finMsg = $('#overlay')
        finMsg.append($("<div class='message'>"))
-       finMsg.text("**" +winner+ " wins!**")
-       finMsg.css({
-          'font-family' : 'Bevan',
-          'color' : 'silver',
+       finMsg.text("**" +winner+ " wins!**")          //This function is called once a win or a tie is identified,
+       finMsg.css({                                   //appends the ending message to the overlay div, and finally
+          'font-family' : 'Bevan',                    //reveals the overlay. Tiles become unclickable once the overlay
+          'color' : 'silver',                         //appears.
            'font-size' : '150px'
         });
          overlay.show();
@@ -203,43 +206,7 @@
 
 
 
-          // var docOverlay = function(){
-          //   $(document).height();
-
-          // $("body").append("<div id='overlay'></div>");
-
-          //  $("#overlay").height(docOverlay);
-         
-
-          //   }
-
-
-
-   // function checkForWinner(){
-   //          var winner = 0;
-   //        if(game_board).find('#box1').hasClass('row1 col1') && 
-   //               (game_board).find('#box2').hasClass('row1 col2') &&
-   //               (game_board).find('#box3').hasClass('row1 col3')){
-   //            console.log("Winner is one!");
-   //          }
-   //       }
-
-
-     // $(box1).on('click' , function(){
- 
-     // 	$(this).toggleClass('box1');
-     // 	var playerSun = $("<div class='Sun'></div>");
-     // 	$(box1).append(playerSun);
-
-     // });
-
-
-     // $(box2).on('click' , function(square){
-     // 	square.preventDefault();
-     // 	$(this).toggleClass('box2');
-     // 	var playerMoon = $("<div class='Moon'></div>");
-     // 	$(box2).append(playerMoon);
-     // });
+     
 
 
 
